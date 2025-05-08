bfc3626272547a2f487221860a11ef16fbca6fab
Matthias Leiter <anonym@nobody.com>
2025-05-08 13:56:08 +0200

    carousel

---
+++ carousel.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const imageFolder = './images/';
+    const totalImages = 3;
+  
+    function loadCarouselImages() {
+      const carouselContainer = document.getElementById('carouselImages');
+      for (let i = 1; i <= totalImages; i++) {
+        const div = document.createElement('div');
+        div.classList.add('carousel-item');
+        if (i === 1) div.classList.add('active');
+  
+        const img = document.createElement('img');
+        img.src = imageFolder + 'img' + i + '.jpeg';
+        img.alt = 'Bild ' + i;
+        
+        img.style.objectFit = 'contain';  
+        img.style.width = '100%';   
+        img.style.height = '100%'; 
+  
+        div.appendChild(img);
+        carouselContainer.appendChild(div);
+      }
+    }
+  
+    loadCarouselImages();
+  });
+  
