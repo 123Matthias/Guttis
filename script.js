49f8689f126211a7fbde034348204e13f96b8ecf
Matthias Leiter <anonym@nobody.com>
2025-05-16 20:48:53 +0200

    style

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
     let logo = document.getElementById("logo");
     
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
       document.body.classList.add("scrolled");
     } else {
       document.body.classList.remove("scrolled");
