f87496cd255171e1767c4073b7cf29f8101a9aa3
Matthias Leiter <anonym@nobody.com>
2025-05-26 15:10:42 +0200

    code refactoring carousel.js

--- carousel.js
+++ carousel.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const imageFolder = './images/carousel/';
-    const totalImages = 23;
-  
-    function loadCarouselImages() {
-      const carouselContainer = document.getElementById('carouselImages');
-      for (let i = 1; i <= totalImages; i++) {
-        const div = document.createElement('div');
-        div.classList.add('carousel-item');
-        if (i === 1) div.classList.add('active');
-  
-        const img = document.createElement('img');
-        img.src = imageFolder + 'img' + i + '.jpg';
-        img.alt = 'Bild ' + i;
 
-        img.loading = 'lazy';
-        
-        img.style.objectFit = 'contain';  
-        img.style.width = '100%';   
-        img.style.height = '100%'; 
-        img.style.zIndex = 2;
-  
-        div.appendChild(img);
-        carouselContainer.appendChild(div);
-      }
-    }
+const imageFolder = './images/carousel/';
+const totalImages = 23;
+
+document.addEventListener('DOMContentLoaded', function () {
     loadCarouselImages();
   });
   
+function loadCarouselImages() {
+const carouselContainer = document.getElementById('carouselImages');
+  for (let i = 1; i <= totalImages; i++) {
+    const div = document.createElement('div');
+    div.classList.add('carousel-item');
+    if (i === 1) div.classList.add('active');
+
+    const img = document.createElement('img');
+    img.src = imageFolder + 'img' + i + '.jpg';
+    img.alt = 'Bild ' + i;
+
+    img.loading = 'lazy';
+    
+    img.style.objectFit = 'contain';  
+    img.style.width = '100%';   
+    img.style.height = '100%'; 
+    img.style.zIndex = 2;
+
+    div.appendChild(img);
+    carouselContainer.appendChild(div);
+  }
+}
