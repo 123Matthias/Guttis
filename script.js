f1e4053c3b625b6d3a06194af24125a2df8cda4d
Matthias Leiter <anonym@nobody.com>
2025-05-29 19:21:22 +0200

    color small displays. fade-in-up animation bei scroll down

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-window.onscroll = function() {
-    let logo = document.getElementById("logo");
-    
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      document.body.classList.add("scrolled");
-    } else {
-      document.body.classList.remove("scrolled");
-    }
-  };
-  
+window.onscroll = function () {
+  let logo = document.getElementById("logo");
 
-  document.addEventListener('DOMContentLoaded', function(){
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.body.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+  }
+};
 
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  addFadeToList();
+  cardObserver();
+  contentFade();
+
+});
+
+
+function addFadeToList() {
+  document.querySelectorAll("li").forEach(element => {
+    element.classList.add("fade-in-up");
+  });
+}
+
+function cardObserver() {
   const card = document.querySelector('#footer-card');
   const link = document.querySelector('#contact-download');
 
@@ -28,4 +42,41 @@ window.onscroll = function() {
   });
 
   observer.observe(card);
+}
+
+
+function contentFade() {
+  const elements = document.querySelectorAll('.fade-in-up');
+  console.log("Scroll in Y = " + window.scrollY);
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+
+    });
+  }, { threshold: 0.2 });
+
+  window.addEventListener('scroll', () => {
+    // console.log("Scroll in Y = " + window.scrollY);
+    if (window.scrollY <= 0) {
+      document.querySelectorAll('.fade-in-up.visible').forEach(el => {
+        el.classList.remove('visible');
+      });
+
+      const elements = document.querySelectorAll('.fade-in-up');
+      elements.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+          el.classList.add('visible');
+        }
+      });
+
+    }
   });
+
+
+  elements.forEach(el => observer.observe(el));
+
+}
+
