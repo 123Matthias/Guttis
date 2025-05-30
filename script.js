d3e8566ffa33062e6fd34ced7ad423f56c11b058
Matthias Leiter <anonym@nobody.com>
2025-05-30 10:00:48 +0200

    Tags verbessert für SEO und reload Website verbessert

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ window.onscroll = function () {
 
 document.addEventListener('DOMContentLoaded', function () {
   addFadeToList();
-  cardObserver();
   contentFade();
+  cardObserver();
 });
 
 window.addEventListener('resize', contentFade);
@@ -54,5 +54,44 @@ function contentFade() {
   }, { threshold: 0, rootMargin: '0px 0px 50px 0px' });
 
   elements.forEach(el => observer.observe(el));
+
+
 }
+
+
+function contentFade() {
+  const elements = document.querySelectorAll('.fade-in-up');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, { threshold: 0, rootMargin: '0px 0px 50px 0px' });
+
+  elements.forEach(el => observer.observe(el));
+
+}
+
+
+// Wenn Seite komplett geladen + Scrollposition wiederhergestellt
+window.addEventListener('pageshow', () => {
+  requestAnimationFrame(makeTopOverflowVisible);
+});
+
+function makeTopOverflowVisible() {
+  const scrollY = window.scrollY || window.pageYOffset || document.documentElement.scrollTop || 0;
+  console.log("psition Y: " + scrollY);
+  
+  const triggerLine = window.scrollY + window.innerHeight; // Unterkante sichtbarer Bereich
+  const elements = document.querySelectorAll('.fade-in-up');
+  elements.forEach(el => {
+    const elTop = el.getBoundingClientRect().top + window.scrollY;
+    if (elTop < triggerLine) {
+      // Element ist schon im sichtbaren Bereich oder oberhalb davon (geladen)
+      el.classList.add('visible');
+    }
+  });
+}
+
 
